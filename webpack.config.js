a3c81c21cb3b33d917de76b3613661882a93a2fb
Chris Flowers <clflowers5@gmail.com>
2018-04-28 20:39:06 -0700

    remove extractTextWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
